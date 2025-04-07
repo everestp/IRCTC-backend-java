8368f83547171fc7c351a775a1f4e7100da8f7e3
Everest Paudel <paudeleverest09@gmail.com>
2025-04-07 18:06:32 +0545

    IRCTC

---
+++ app/src/main/java/org/example/services/UserBookingService.java
@@ -0,0 +1,11 @@
+package org.example.services;
+
+import org.example.entities.User;
+
+public class UserBookingService {
+    private User user;
+    public  UserBookingService(User user){
+        this.user =user;
+    }
+
+}
